4a212efad6064db41bb0cf56d8b77992621873f6
Pranathi Poojary <pranathi.poojary20@gmail.com>
2025-03-20 15:31:16 +0000

    Initial Commit

---
+++ Life/LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeApp.swift
+//  Life
+//
+//  Created by Pranathi Poojary on 20/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LifeTests/LifeTests.swift
@@ -0,0 +1,16 @@
+//
+//  LifeTests.swift
+//  LifeTests
+//
+//  Created by Pranathi Poojary on 20/03/2025.
+//
+
+import Testing
+
+struct LifeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
